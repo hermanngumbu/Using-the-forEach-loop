352cc93569a4991e83d215adbbbf91f078365faa
Herman <hermanngumbu@gmail.com>
2022-08-15 11:59:32 +0100

    Added app.js file with object table

---
+++ app.js
@@ -0,0 +1,31 @@
+const contacts = [{
+        id: 1,
+        nom: 'Ngumbu',
+        prenom: 'Herman',
+        email: 'hermanngumbu@gmail.com',
+        age: 12,
+    },
+    {
+        id: 2,
+        nom: 'Kabashi',
+        prenom: 'Romain',
+        email: 'romainkabashi@gmail.com',
+        age: 22,
+    },
+    {
+        id: 3,
+        nom: 'Rashidi',
+        prenom: 'Lydie',
+        email: 'lydie.rashidi@yahoo.fr',
+        age: 43,
+    },
+    {
+        id: 4,
+        nom: 'Matondo',
+        prenom: 'Jules',
+        email: 'jules@yahoo.com',
+        age: 10,
+    },
+];
+
+const myTableBody = document.querySelector('')
